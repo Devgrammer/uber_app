23fa98abe366a7cb16b4cc2fc4c0829feed19031
Abhinav Rastogi <rastogiabhinav@Abhinavs-MacBook-Pro.local>
2025-02-18 19:35:06 +0530

    Build issue fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base:'./',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html' // Ensure entry HTML is considered
+    }
+  },
   plugins: [react(), tailwindcss()],
   
 })
